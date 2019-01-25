4f1ba9b4760c16a9fd5ae2cb4ec56085014c1e82
k5md <serg.dfc@yandex.ru>
2019-01-25 15:06:28 -0500

    prepared tests for the next step

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,18 +1,20 @@
 import path from 'path';
 import fs from 'fs';
-import _ from 'lodash';
+import cartesian from 'cartesian';
 import genDiff from '../src';
 
 const supportedExtensions = ['json', 'yml', 'ini'];
+const outputFormats = ['Plain', 'General'];
 const types = ['flat', 'nested'];
-const pairs = _.flatten(supportedExtensions.map(extension => types.map(type => [extension, type])));
 
-test.each(pairs)(
-  'outputs the difference between %s %s',
-  (extension, type) => {
+const groups = cartesian([supportedExtensions, outputFormats, types]);
+
+test.each(groups)(
+  'outputs the difference between %s %s %s',
+  (extension, format, type) => {
     const firstConfig = path.resolve(__dirname, `__fixtures__/${type}/before.${extension}`);
     const secondConfig = path.resolve(__dirname, `__fixtures__/${type}/after.${extension}`);
-    const fixturePath = path.resolve(__dirname, `__fixtures__/${type}/${extension}Fixture.txt`);
+    const fixturePath = path.resolve(__dirname, `__fixtures__/${type}/${extension}Fixture${format}.txt`);
 
     const expectedDiff = fs.readFileSync(fixturePath, 'utf-8');
     const actualDiff = genDiff(firstConfig, secondConfig);
