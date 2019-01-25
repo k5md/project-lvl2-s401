6be847625a9e2960dfb64d58d734c7f2edb80361
k5md <serg.dfc@yandex.ru>
2019-01-25 10:26:19 -0500

    almost managed to separate common functionality in renderIter

--- src/render.js
+++ src/render.js
@@ -6,30 +6,41 @@ const stringify = (level, object) => {
   if (_.isObject(object)) {
     return _
       .toPairs(object)
-      .map(([key, value]) => `{\n${indent(level + 3, '')}${key}: ${stringify(level, value)}\n${indent(level + 1, '}')}`);
+      .map(([key, value]) => `{\n${indent(level, '')}${key}: ${stringify(level, value)}\n${indent(level + 1, '}')}`);
   }
   return object;
 };
 
-const renderIter = (level, ast, operations) => {
+const renderIter = (level, ast) => {
+  const operations = {
+    composite:
+      (level, { key, children }) => {
+        return [
+          indent(level, `  ${key}: {`),
+          indent(level, `  ${renderIter(level + 2, children)}`),
+          indent(level, '  }'),
+        ];
+      },
+    changed:
+      (level, { key, value: [value1, value2] }) => {
+        return [
+          indent(level, `- ${key}: ${stringify(level, value1)}`), 
+          indent(level, `+ ${key}: ${stringify(level, value2)}`),
+        ]
+      },
+    removed:
+      (level, { key, value }) => indent(level, `- ${key}: ${stringify(level, value)}`),
+    added:
+      (level, { key, value }) => indent(level, `+ ${key}: ${stringify(level, value)}`),
+    unchanged:
+      (level, { key, value }) => indent(level, `  ${key}: ${stringify(level, value)}`),
+  };
+
   const diffStrings = ast.map(node => operations[node.type](level, node));
 
   return _.flatten(diffStrings).join('\n');
 };
 
-const operations = {
-  composite:
-    (level, { key, children }) => indent(level + 1, `${key}: {\n${(renderIter(level + 2, children, operations))}\n${indent(level + 1, '}')}`),
-  changed:
-    (level, { key, value: [value1, value2] }) => [indent(level, `- ${key}: ${stringify(level, value1)}`), indent(level, `+ ${key}: ${stringify(level, value2)}`)],
-  removed:
-    (level, { key, value }) => indent(level, `- ${key}: ${stringify(level, value)}`),
-  added:
-    (level, { key, value }) => indent(level, `+ ${key}: ${stringify(level, value)}`),
-  unchanged:
-    (level, { key, value }) => indent(level, `  ${key}: ${stringify(level, value)}`),
-};
-
-const render = ast => `{\n${renderIter(1, ast, operations)}\n}`;
+const render = ast => `{\n${renderIter(1, ast)}\n}`;
 
 export default render;
